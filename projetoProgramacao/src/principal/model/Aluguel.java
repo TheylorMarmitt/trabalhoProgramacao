186c6029f714c83825d29d2874d3f3288fd51160
Luciano Virmes <luciano.jp@unoesc.edu.br>
2018-09-04 20:56:39 -0300

    Adicionados atributos na classe Aluguel

--- projetoProgramacao/src/principal/model/Aluguel.java
+++ projetoProgramacao/src/principal/model/Aluguel.java
@@ -7,12 +7,71 @@ public class Aluguel {
 		private int codigo;
 		private LocalDate dataAluguel;
 		private LocalDate dataDevolucao;
+		private Double quilometrosEntrada;
+		private Double quilometrosSaida;
+		
+		private Carro carro = new Carro();
+		private Filial filial = new Filial();
+		private Vendedor vendedor = new Vendedor();
+		private TipoAluguel tipoAluguel = new TipoAluguel();
+		 
+		public boolean confirmaAluguel(){
+			return false;
+		}
+		public Double calculaQuilometros() {
+			return null;
+		}
+		
 		
 		
 		public Aluguel() {
 		
 		}
 		
+		public Double getQuilometrosEntrada() {
+			return quilometrosEntrada;
+		}
+		public void setQuilometrosEntrada(Double quilometrosEntrada) {
+			this.quilometrosEntrada = quilometrosEntrada;
+		}
+		public Double getQuilometrosSaida() {
+			return quilometrosSaida;
+		}
+		public void setQuilometrosSaida(Double quilometrosSaida) {
+			this.quilometrosSaida = quilometrosSaida;
+		}
+		public Carro getCarro() {
+			return carro;
+		}
+
+		public void setCarro(Carro carro) {
+			this.carro = carro;
+		}
+
+		public Filial getFilial() {
+			return filial;
+		}
+
+		public void setFilial(Filial filial) {
+			this.filial = filial;
+		}
+
+		public Vendedor getVendedor() {
+			return vendedor;
+		}
+
+		public void setVendedor(Vendedor vendedor) {
+			this.vendedor = vendedor;
+		}
+
+		public TipoAluguel getTipoAluguel() {
+			return tipoAluguel;
+		}
+
+		public void setTipoAluguel(TipoAluguel tipoAluguel) {
+			this.tipoAluguel = tipoAluguel;
+		}
+
 		public int getCodigo() {
 			return codigo;
 		}
