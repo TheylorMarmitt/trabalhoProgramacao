57f9e8b54c0d0f27cabcf9cf6fc1d36f8532bb72
Luciano Virmes <luciano.jp@unoesc.edu.br>
2018-09-11 15:47:29 -0300

    Criado os formularios de cadastro de cliente, aluguel, filial  e funcionario

--- projetoProgramacao/src/principal/dao/TipoAluguelJDBC.java
+++ projetoProgramacao/src/principal/dao/TipoAluguelJDBC.java
@@ -8,31 +8,26 @@ public class TipoAluguelJDBC implements TipoAluguelDAO{
 
 	@Override
 	public void inserir(TipoAluguel dado) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void alterar(TipoAluguel dado) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void excluir(TipoAluguel dado) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public List<TipoAluguel> listar() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public TipoAluguel buscar(Integer codigo) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
