23560163eb2f31fb0eaca35d6656296782458416
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-11 23:00:30 -0300

    ajustes

--- projetoProgramacao/src/principal/model/Cliente.java
+++ projetoProgramacao/src/principal/model/Cliente.java
@@ -9,63 +9,7 @@ public class Cliente extends Pessoa {
 	private LocalDate dataDeCadastro;
 	private String cnh;
 
-	/**
-	 * 
-	 * @return true se cpf for valido
-	 */
-	public Boolean validaCpf() {
-		boolean cpfValido = false;
-		int decimaPosicao = 10;
-		int total = 0;
-		char[] chars = this.getCpf().toCharArray();
-
-		for (int i = 0; i < 9; i++) {
-			int valorCaracterEmPosicao = Integer.valueOf(String.valueOf(chars[i]));
-			total = total + (valorCaracterEmPosicao * decimaPosicao);
-			decimaPosicao--;
-		}
-
-		int validacao1 = total % 11;
-		validacao1 = 11 - validacao1;
-		if (validacao1 > 9) {
-			validacao1 = 0;
-		}
-
-		int valorCaracterEmPosicao9 = Integer.valueOf(String.valueOf(chars[9]));
-
-		if (validacao1 == valorCaracterEmPosicao9) {
-			cpfValido = true;
-		}
-
-		int decimaPrimeiraPosicao = 11;
-		total = 0;
-		for (int i = 0; i < 10; i++) {
-			int valorCaracterEmPosicao = Integer.valueOf(String.valueOf(chars[i]));
-			total = total + (valorCaracterEmPosicao * decimaPrimeiraPosicao);
-			decimaPrimeiraPosicao--;
-		}
-
-		int validacao2 = total % 11;
-		validacao2 = 11 - validacao2;
-
-		if (validacao2 > 9) {
-			validacao2 = 0;
-		}
-
-		int valorCaracterEmPosicao10 = Integer.valueOf(String.valueOf(chars[10]));
-
-		if (validacao2 == valorCaracterEmPosicao10) {
-			cpfValido = true;
-		}
-
-		if (!cpfValido) {
-			return false;
-		} else {
-			return true;
-		}
-
-	}
-
+	
 	/**
 	 * 
 	 * @return idade inteiro
