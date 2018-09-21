99a82d9ca30e3c50bb4fc6e98111f2c649ad047d
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-09-21 17:01:32 -0300

    Tela cadastro carro (FALTA TERMINAR)

---
+++ projetoProgramacao/src/principal/CadastroCarroController.java
@@ -0,0 +1,53 @@
+package principal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CadastroCarroController {
+
+	 @FXML
+	    private ComboBox<?> cbFilial;
+
+	    @FXML
+	    private TextField tfMarca;
+
+	    @FXML
+	    private TextField tfModelo;
+
+	    @FXML
+	    private TextField tfValor;
+
+	    @FXML
+	    private TextField tfCor;
+
+	    @FXML
+	    private DatePicker dtAno;
+
+	    @FXML
+	    private TextField tfPlaca;
+
+	    @FXML
+	    private CheckBox chekDisponivel;
+
+	    @FXML
+	    private Button btnCadastrar;
+
+	    @FXML
+	    private Button btnNovo;
+
+	    @FXML
+	    void cadastrar(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void novo(ActionEvent event) {
+
+	    }
+
+}
