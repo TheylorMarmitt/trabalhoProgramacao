bf6778effa7536c391755c782a6dba74c6ce466f
Luciano Virmes <luciano.jp@unoesc.edu.br>
2018-09-24 09:09:56 -0300

    Alteração dos nomes

--- projetoProgramacao/src/principal/CadastroVeiculoController.java
+++ projetoProgramacao/src/principal/CadastroVeiculoController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
-public class CadastroCarroController {
+public class CadastroVeiculoController {
 
 	 @FXML
 	    private ComboBox<?> cbFilial;
